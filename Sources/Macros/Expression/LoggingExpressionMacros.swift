6a37d80bbe34e9c26579f5e2ffc73fd21a553a98
minsOne <jungminahn.minsone@gmail.com>
2024-03-13 01:07:08 +0900

    modify error message

--- Sources/Macros/Expression/LoggingExpressionMacros.swift
+++ Sources/Macros/Expression/LoggingExpressionMacros.swift
@@ -1,5 +1,6 @@
 import SwiftDiagnostics
 import SwiftSyntax
+import SwiftSyntaxMacroExpansion
 import SwiftSyntaxMacros
 
 /// OS 버전에 따라 Logger의 호출이 달라지므로, 조건에 따라 변경이 필요함
@@ -7,14 +8,16 @@ public struct LoggingExpressionMacros: ExpressionMacro {
     public static func expansion(
         of node: some FreestandingMacroExpansionSyntax,
         in context: some MacroExpansionContext
-    ) -> ExprSyntax {
+    ) throws -> ExprSyntax {
         let argumentList = node.argumentList
         guard
             argumentList.count == 2,
             let firstElement = argumentList.first,
             let secondElement = argumentList.last
         else {
-            fatalError("compiler bug: the macro does not have any arguments")
+            throw MacroExpansionErrorMessage(
+                "log는 OSLogType, message이 필요합니다. 확인해주세요"
+            )
         }
 
         let oslogType = firstElement.expression
@@ -26,10 +29,15 @@ public struct LoggingExpressionMacros: ExpressionMacro {
             .as(StringSegmentSyntax.self)?
             .content.text
 
-        guard
-            let oslogType, let msg
-        else {
-            fatalError("compiler bug: the macro does not have any arguments")
+        guard let oslogType else {
+            throw MacroExpansionErrorMessage(
+                "log는 OSLogType, message이 필요합니다. 확인해주세요"
+            )
+        }
+        guard let msg else {
+            throw MacroExpansionErrorMessage(
+                "message 타입이 String인지 확인해주세요"
+            )
         }
 
         return "\(raw: generateExpr(oslogType: oslogType, msg: msg))"
